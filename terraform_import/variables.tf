cd3a96ab608077163739794b183da7ac582ec9da
Mbayang <mdiop@bluesquarehub.com>
2021-05-19 13:41:27 +0000

    Define default value of backup variables to true into terraform_import

--- terraform_import/variables.tf
+++ terraform_import/variables.tf
@@ -23,11 +23,11 @@ variable "gcp_sql_instance_availability_type" {
 }
 variable "gcp_sql_instance_backup_enabled" {
   description = "True if backup configuration is enabled"
-  default     = false
+  default     = true
 }
 variable "gcp_sql_instance_point_in_time_recovery_enabled" {
   description = "True if Point-in-time recovery is enabled"
-  default     = false
+  default     = true
 }
 # GKE cluster
 variable "gcp_gke_cluster_name" {
