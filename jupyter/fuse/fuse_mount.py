3b2701b9bf0a2b7fe7ad7fbafd79d6af64aefda7
pvanliefland <pierre.vanliefland@gmail.com>
2021-09-10 14:20:40 +0200

    HEXA-335 Use s3fuse instead of s3fs?
    Check feature flag

--- jupyter/fuse/fuse_mount.py
+++ jupyter/fuse/fuse_mount.py
@@ -1,24 +1,26 @@
 import os
 import subprocess
 
-os.putenv("AWSACCESSKEYID", os.environ["AWS_ACCESS_KEY_ID"])
-os.putenv("AWSSECRETACCESSKEY", os.environ["AWS_SECRET_ACCESS_KEY"])
-os.putenv("AWSSESSIONTOKEN", os.environ["AWS_SESSION_TOKEN"])
-for bucket_name in os.environ.get("AWS_S3_BUCKET_NAMES", "").split(","):
-    path_to_mount = os.path.join(f"/home/jovyan/s3fs:{bucket_name}")
-    subprocess.run(["mkdir", "-p", path_to_mount])
-    subprocess.run(
-        [
-            "s3fs",
-            bucket_name,
-            path_to_mount,
-            "-o",
-            "allow_other",
-            # Debug
-            # "-o",
-            # "dbglevel=info",
-            # "-f",
-            # "-o",
-            # "curldbg",
-        ]
-    )
+# TODO: upgrade to more recent version of s3fs, which handles the standard AWS env variables just fine
+if os.environ.get("HEXA_FEATURE_FLAG_S3FS", "false") == "true":
+    os.putenv("AWSACCESSKEYID", os.environ["AWS_ACCESS_KEY_ID"])
+    os.putenv("AWSSECRETACCESSKEY", os.environ["AWS_SECRET_ACCESS_KEY"])
+    os.putenv("AWSSESSIONTOKEN", os.environ["AWS_SESSION_TOKEN"])
+    for bucket_name in os.environ.get("AWS_S3_BUCKET_NAMES", "").split(","):
+        path_to_mount = os.path.join(f"/home/jovyan/s3-{bucket_name}")
+        subprocess.run(["mkdir", "-p", path_to_mount])
+        subprocess.run(
+            [
+                "s3fs",
+                bucket_name,
+                path_to_mount,
+                "-o",
+                "allow_other",
+                # Debug
+                # "-o",
+                # "dbglevel=info",
+                # "-f",
+                # "-o",
+                # "curldbg",
+            ]
+        )
