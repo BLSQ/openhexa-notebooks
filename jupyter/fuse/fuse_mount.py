f963da9b597b6c974c06bd8ed6ac0c8b477d9932
hastakecorp <github@corp.hastake.com>
2021-10-27 17:26:00 +0200

    [fix] use new variable format

--- jupyter/fuse/fuse_mount.py
+++ jupyter/fuse/fuse_mount.py
@@ -1,21 +1,30 @@
 import os
 import subprocess
+import json
+import base64
 
 # TODO: upgrade to more recent version of s3fs, which handles the standard AWS env variables just fine
 if os.environ.get("HEXA_FEATURE_FLAG_S3FS", "false") == "true":
-    os.putenv("AWSACCESSKEYID", os.environ["AWS_ACCESS_KEY_ID"])
-    os.putenv("AWSSECRETACCESSKEY", os.environ["AWS_SECRET_ACCESS_KEY"])
-    os.putenv("AWSSESSIONTOKEN", os.environ["AWS_SESSION_TOKEN"])
-    for bucket_name in os.environ.get("AWS_S3_BUCKET_NAMES", "").split(","):
-        path_to_mount = os.path.join(f"/home/jovyan/s3-{bucket_name}")
-        subprocess.run(["mkdir", "-p", path_to_mount])
+    fuse_config = json.loads(base64.b64decode(os.environ["_PRIVATE_FUSE_CONFIG"]))
+
+    os.putenv("AWSACCESSKEYID", fuse_config["access_key_id"])
+    os.putenv("AWSSECRETACCESSKEY", fuse_config["secret_access_key"])
+    os.putenv("AWSSESSIONTOKEN", fuse_config["session_token"])
+
+    for bucket in fuse_config["buckets"]:
+        path_to_mount = os.path.join(f"/home/jovyan/s3-{bucket['name']}")
+        region_url = f"https://s3-{bucket['region']}.amazonaws.com/"
+        subprocess.run(["echo", "mkdir", "-p", path_to_mount])
         subprocess.run(
             [
+                "echo",
                 "s3fs",
-                bucket_name,
+                bucket["name"],
                 path_to_mount,
                 "-o",
                 "allow_other",
+                "-o",
+                "url=" + region_url,
                 # Debug
                 # "-o",
                 # "dbglevel=info",
