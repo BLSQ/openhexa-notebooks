79203db85bae7f73e3383ae1760d73ea8af93148
hastakecorp <github@corp.hastake.com>
2022-04-25 15:26:14 +0200

    fixup! Add support for GoogleCloudStorage fuse mount  and refactor S3 credentials

--- jupyter/fuse/fuse_mount.py
+++ jupyter/fuse/fuse_mount.py
@@ -10,7 +10,7 @@ os.putenv("AWSSESSIONTOKEN", os.environ["AWS_SESSION_TOKEN"])
 
 buckets_ro = os.environ.get("AWS_S3_BUCKET_RO_NAMES", "").split(",")
 
-for bucket_name in os.environ.get("AWS_S3_BUCKET_NAMES", "").split(","):
+for bucket_name in filter(None, os.environ.get("AWS_S3_BUCKET_NAMES", "").split(",")):
     path_to_mount = f"/home/jovyan/s3-{bucket_name}"
     subprocess.run(["mkdir", "-p", path_to_mount])
     subprocess.run(
@@ -32,9 +32,9 @@ for bucket_name in os.environ.get("AWS_S3_BUCKET_NAMES", "").split(","):
 gcsfuse_config_file = f"/home/jovyan/.gcsfuse.json"
 os.putenv("GOOGLE_APPLICATION_CREDENTIALS", gcsfuse_config_file)
 with open(gcsfuse_config_file, "w") as fd:
-    fd.write( base64.b64decode(os.environ["GCS_CREDENTIALS"].encode()).decode() );
+    fd.write( base64.b64decode(os.environ.get("GCS_CREDENTIALS", "").encode()).decode() );
 
-for bucket_name in os.environ.get("GCS_BUCKET_NAMES", "").split(","):
+for bucket_name in filter(None, os.environ.get("GCS_BUCKET_NAMES", "").split(",")):
     path_to_mount = f"/home/jovyan/gcs-{bucket_name}"
     subprocess.run(["mkdir", "-p", path_to_mount])
     subprocess.run(
@@ -44,4 +44,3 @@ for bucket_name in os.environ.get("GCS_BUCKET_NAMES", "").split(","):
             path_to_mount,
         ]
     )
-
--- jupyter/fuse/fuse_umount.py
+++ jupyter/fuse/fuse_umount.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-umount_list = os.environ.get("AWS_S3_BUCKET_NAMES", "").split(",") + os.environ.get("GCS_BUCKET_NAMES", "").split(",")
+umount_list = filter(None, os.environ.get("AWS_S3_BUCKET_NAMES", "").split(",") + os.environ.get("GCS_BUCKET_NAMES", "").split(","))
 for bucket_name in umount_list:
     path_to_umount = f"/home/jovyan/s3-{bucket_name}"
     subprocess.run(
