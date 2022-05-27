a21bf47466b6eeec39ad9b97401b7323a78d6c89
hastakecorp <github@corp.hastake.com>
2022-05-27 12:28:56 +0200

    [ref] clean up s3content manager, use fuse instead

--- jupyter/jupyter_notebook_config.py
+++ jupyter/jupyter_notebook_config.py
@@ -1,24 +1,10 @@
 import os
 
-from notebook.services.contents.largefilemanager import LargeFileManager
-from hybridcontents import HybridContentsManager
-
 c = get_config()
 
 # Iframe stuff
 c.NotebookApp.tornado_settings = {
     "headers": {"Content-Security-Policy": os.environ["CONTENT_SECURITY_POLICY"]}
-}
-
-# Contents managers
-# We start with a LargeFileManager at the root, and we will mount other managers depending on
-# user permissions
-c.NotebookApp.contents_manager_class = HybridContentsManager
-c.HybridContentsManager.manager_classes = {
-    "": LargeFileManager,
-}
-c.HybridContentsManager.manager_kwargs = {
-    "": {},
 }
 
 # Fix for https://github.com/BLSQ/habari/issues/31
