2b84069bbec05759b4b5542064fe1244e75e2340
pvanliefland <pierre.vanliefland@gmail.com>
2022-08-16 10:02:10 +0200

    Hack fuse_mount.py to detect git extension feature flag and deactivate if needed

--- jupyter/fuse/fuse_mount.py
+++ jupyter/fuse/fuse_mount.py
@@ -6,6 +6,14 @@ import http.server
 import multiprocessing
 import time
 
+# Git plugin / deactivate if no feature flag
+# TODO: this has nothing to do with Fuse, this file should be reworked
+if not os.environ.get("GIT_EXTENSION_ENABLED", "false") == "true":
+    subprocess.run(
+        ["/opt/conda/bin/jupyter", "labextension", "disable", "@jupyterlab/git"]
+    )
+
+# S3 Fuse
 aws_fuse_config = json.loads(base64.b64decode(os.environ["AWS_S3_FUSE_CONFIG"]))
 os.putenv("AWSACCESSKEYID", aws_fuse_config["AWS_ACCESS_KEY_ID"])
 os.putenv("AWSSECRETACCESSKEY", aws_fuse_config["AWS_SECRET_ACCESS_KEY"])
@@ -30,10 +38,12 @@ for bucket in filter(None, aws_fuse_config["buckets"]):
             # "-f",
             # "-o",
             # "curldbg",
-        ] + (["-o", "ro"] if bucket["mode"] == "RO" else [])
+        ]
+        + (["-o", "ro"] if bucket["mode"] == "RO" else [])
     )
 
-# gcsfuse offers 2 choises to authenticate:
+# GCS Fuse
+# gcsfuse offers 2 choices to authenticate:
 # 1) using a 'JSON key file', with static credentials
 # 2) using a token (that can be a short lived one) served over HTTP
 # So we span an HTTP server to use the second option.
@@ -41,6 +51,7 @@ for bucket in filter(None, aws_fuse_config["buckets"]):
 GCS_TOKEN = os.environ.get("GCS_TOKEN", "")
 
 if GCS_TOKEN:
+
     class serveGCStoken(http.server.BaseHTTPRequestHandler):
         def do_GET(self):
             self.send_response(200)
@@ -48,27 +59,26 @@ if GCS_TOKEN:
             self.end_headers()
             self.wfile.write(bytes('{ "access_token": "' + GCS_TOKEN + '" }', "utf-8"))
 
-    webServer = http.server.HTTPServer(('127.0.0.1',4321), serveGCStoken)
+    webServer = http.server.HTTPServer(("127.0.0.1", 4321), serveGCStoken)
     x = multiprocessing.Process(target=webServer.serve_forever, args=())
     x.start()
     time.sleep(1)
 
     # b64("{}") == b'e30='
-    gcsfuse_buckets = json.loads(base64.b64decode(os.environ.get("GCS_BUCKETS", b'e30=')))
+    gcsfuse_buckets = json.loads(
+        base64.b64decode(os.environ.get("GCS_BUCKETS", b"e30="))
+    )
     for bucket in filter(None, gcsfuse_buckets.get("buckets", [])):
         path_to_mount = f"/home/jovyan/gcs-{bucket['name']}"
         subprocess.run(["mkdir", "-p", path_to_mount])
         args = [
-                "gcsfuse",
-                "-o",
-                "allow_other",
+            "gcsfuse",
+            "-o",
+            "allow_other",
         ]
         args.extend(["-o", "ro"] if bucket["mode"] == "RO" else [])
-        args.extend([
-                "--token-url",
-                "http://127.0.0.1:4321/",
-                bucket["name"],
-                path_to_mount
-        ])
+        args.extend(
+            ["--token-url", "http://127.0.0.1:4321/", bucket["name"], path_to_mount]
+        )
         subprocess.run(args)
     x.terminate()
