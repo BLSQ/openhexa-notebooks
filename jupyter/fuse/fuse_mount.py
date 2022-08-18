da1c5d7fc37fdeefeb8edf60a248078ec54939f1
hastakecorp <github@corp.hastake.com>
2022-08-18 14:56:25 +0200

    [enh] better support of conditionnal config

--- jupyter/fuse/fuse_mount.py
+++ jupyter/fuse/fuse_mount.py
@@ -14,12 +14,15 @@ if not os.environ.get("GIT_EXTENSION_ENABLED", "false") == "true":
     )
 
 # S3 Fuse
-aws_fuse_config = json.loads(base64.b64decode(os.environ["AWS_S3_FUSE_CONFIG"]))
-os.putenv("AWSACCESSKEYID", aws_fuse_config["AWS_ACCESS_KEY_ID"])
-os.putenv("AWSSECRETACCESSKEY", aws_fuse_config["AWS_SECRET_ACCESS_KEY"])
-os.putenv("AWSSESSIONTOKEN", aws_fuse_config["AWS_SESSION_TOKEN"])
+# b64("{}") == b'e30='
+aws_fuse_config = json.loads(
+    base64.b64decode(os.environ.get("AWS_S3_FUSE_CONFIG", b"e30="))
+)
+os.putenv("AWSACCESSKEYID", aws_fuse_config.get("AWS_ACCESS_KEY_ID", ""))
+os.putenv("AWSSECRETACCESSKEY", aws_fuse_config.get("AWS_SECRET_ACCESS_KEY", ""))
+os.putenv("AWSSESSIONTOKEN", aws_fuse_config.get("AWS_SESSION_TOKEN", ""))
 
-for bucket in filter(None, aws_fuse_config["buckets"]):
+for bucket in filter(None, aws_fuse_config.get("buckets", [])):
     path_to_mount = f"/home/jovyan/s3-{bucket['name']}"
     region_url = f"https://s3-{bucket['region']}.amazonaws.com/"
     subprocess.run(["mkdir", "-p", path_to_mount])
