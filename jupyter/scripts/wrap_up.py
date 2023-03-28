5147788a9cbac4dc0d63dc2909ec9f91f92a43ef
Pierre Vanliefland <pvanliefland@users.noreply.github.com>
2023-03-28 14:52:26 +0200

    Workspaces filesystem fine-tuning (#42)
    Fine-tune permissions on jupyter filesystem in workspaces and add new README.md file

---
+++ jupyter/scripts/wrap_up.py
@@ -0,0 +1,19 @@
+import os, subprocess
+
+if os.environ["OPENHEXA_LEGACY"] == "true":
+    # Copy README
+    subprocess.run(["cp", "/tmp/notebooks_sample_files/legacy/README.md", "/home/jovyan/README.md"])
+else:
+    # Copy README
+    subprocess.run(["cp", "/tmp/notebooks_sample_files/README.md", "/home/jovyan/README.md"])
+
+    # Delete work directory if any, and create tmp instead
+    subprocess.run(["rmdir", "/home/jovyan/work"])
+    subprocess.run(["mkdir", "/home/jovyan/tmp"])
+
+    # Make /home/jovyan read-only
+    subprocess.run(["chmod", "a-w", "/home/jovyan"])
+
+    # Make /home/jovyan/worskpace + /home/jovyan/tmp writable
+    subprocess.run(["chmod", "g+w", "/home/jovyan/workspace"])
+    subprocess.run(["chmod", "g+w", "/home/jovyan/tmp"])
