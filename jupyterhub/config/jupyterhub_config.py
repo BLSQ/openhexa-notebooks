af41f4b2b9d08501d38ab19abe305553b5849f80
Quentin Gérôme <quentin@letsbuildit.dev>
2024-04-18 16:08:52 +0200

    chore(Billing): Add the hexa-workspace to the labels of the pod
    This value was already present in another label but it's to have the same label for all resources

--- jupyterhub/config/jupyterhub_config.py
+++ jupyterhub/config/jupyterhub_config.py
@@ -229,3 +229,6 @@ c.JupyterHub.load_roles.append(
         ],
     }
 )
+
+# Set additional labels on pods (useful for monitoring & billing)
+c.KubeSpawner.extra_labels = {"hexa-workspace": "{servername}"}
